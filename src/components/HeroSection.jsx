6dea6618c538ed493263283e5b095fd3baf64785
Azhar Aufa <azharaufa221105@gmail.com>
2025-01-10 02:41:41 +0700

    Edit Logo and created something

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,15 +4,16 @@ import video2 from "../assets/video2.mp4";
 const HeroSection = () => {
   return (
     <div className="flex flex-col items-center mt-6 lg:mt-20">
+        <h3 className="mt-5 text-lg text-center text-white max-w-4xl">Created by Azhar Aufa</h3>
         <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-            VirtualR build tools 
+            AufalR Code build tools 
             <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">{" "}for developers</span>
         </h1>
         <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
             Empower your creativity and bring your VR app ideas to life with our intuitive development tools. Get started today and turn your imagination into immersive reality!
         </p>
         <div className="flex justify-center my-10">
-            <a href="#" className="bg-gradient-to-r from-orange-500 to-red-800 py-3 px-4 mx-3 rounded-md">
+            <a href="#" className="bg-gradient-to-r from-orang  e-500 to-red-800 py-3 px-4 mx-3 rounded-md">
                 Start for free
             </a>
             <a href="#" className="py-3 px-4 mx-3 rounded-md border">
